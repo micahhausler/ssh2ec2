3d96564bf3d6b3279af56ace5e0b7d08601850c6
Mike Ryan <mike@fadedink.co.uk>
2016-03-18 11:00:24 +0100

    added package to setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ with open('requirements.txt') as f:
 
 setup(
     name="ssh2ec2",
-    version="0.2",
+    version="0.3",
     author="Mike Ryan",
     author_email="mike@awssystemadministration.com",
     description="SSH into EC2 instances via tags and metadata filters",
     license="MIT",
     url="https://github.com/mikery/ssh2ec2",
     keywords=["amazon", "aws", "ec2", "ssh", "cloud", "boto"],
+    packages=['ssh2ec2'],
     install_requires=requirements,
     entry_points={
         'console_scripts': ['ssh2ec2=ssh2ec2:main'],
