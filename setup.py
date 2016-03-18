7f6e3361338fcbaf70c6d178f7bbd7f2dda30460
Mike Ryan <mike@fadedink.co.uk>
2016-03-18 10:38:20 +0100

    added requirements file

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="ssh2ec2",
     version="0.1",
@@ -10,7 +13,7 @@ setup(
     license="MIT",
     url="https://github.com/mikery/ssh2ec2",
     keywords=["amazon", "aws", "ec2", "ssh", "cloud", "boto"],
-    #install_requires = requirements,
+    install_requires=requirements,
     entry_points={
         'console_scripts': ['ssh2ec2=ssh2ec2:main'],
     }
